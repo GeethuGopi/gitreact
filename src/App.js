34984f5db7833ed8041eef4ef7e78ad3ab879c75
Geethu Gopi <geethugopi33@gmail.com>
2020-11-09 02:33:29 +1300

    heading tag is added in contact

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './Component/Home';
 import About from './Component/About';
+import Contact from './Component/Contact';
 const App = () => {
   return ( 
     <div>
     <Home />
     <About />
+    <Contact />
     </div>
    );
 }
