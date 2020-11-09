cd1b2389f9f54ba15490b82d044e81fc4b037fac
Geethu Gopi <geethugopi33@gmail.com>
2020-11-10 11:04:34 +1300

    Navigation removed from each pages and added in app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import {Route, BrowserRouter as Router} from 'react-router-dom';
 import Home from './Component/Home';
 import About from './Component/About';
 import Contact from './Component/Contact';
+import Navigation from './Component/Navigation';
 const App = () => {
   return ( 
     <Router>
+    <Route>
+    <Navigation />
+    </Route>
     <Route exact path="/">
     <Home />
     </Route>
@@ -16,6 +20,7 @@ const App = () => {
     <Contact />
     </Route>
     </Router>
+    
    );
 }
  
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navigation from './Navigation';
+
 const Home = () => {
     return (
         <div>
-        <Navigation />
+        
         <h1>This is home page</h1>
         </div>
       );
